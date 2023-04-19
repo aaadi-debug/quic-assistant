78aa3e9de9d226965ed79339dd0686b6a687927a
Aditya Raj Gupta <73938722+aaadi-debug@users.noreply.github.com>
2023-04-20 01:18:48 +0530

    second commit

--- client/script.js
+++ client/script.js
@@ -174,7 +174,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response 
   //after setting up backend, then we are coding this
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://quic-assistant.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
